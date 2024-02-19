402e748414f6592e1c97a6ba623c02d30b2ab38b
gbl <melbye_law@proton.me>
2024-02-19 16:49:38 -0300

    .

--- database.py
+++ database.py
@@ -2,15 +2,21 @@ import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-MONGO_DB_ATLAS_PASSWORD = os.environ.get("MONGO_DB_ATLAS_PASSWORD")
-uri = f"mongodb+srv://psiqguilhermeletsch:{MONGO_DB_ATLAS_PASSWORD}@my-clinic.kcqzquv.mongodb.net/?retryWrites=true&w=majority"
+def get_db():
+    uri = os.environ.get("MONGO_DB_ATLAS_URI")
+    # uri = "mongodb+srv://psiqguilhermeletsch:icuX44H0nY7NpGXm@my-clinic.kcqzquv.mongodb.net/?retryWrites=true&w=majority"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+    except Exception as e:
+        raise(e)
+
+    return client
+
+if __name__ == "__main__":
+    client = get_db()
+    print(client)
